deda321d1f2af2798875fff7f32b8b1dc59c2f5f
paul-kd <paul.koudolga.m@gmail.com>
2023-12-04 21:10:09 +0000

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+
+function App() {
+    return <Fragment>
+      <h1 id ="title" className="">Bonjour les gens </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Cum corrupti nam, enim blanditiis quaerat tenetur, laborum neque quod molestias dolor quas necessitatibus nobis aut, 
+        accusamus similique temporibus praesentium error beatae!
+      </p>
+    </Fragment>
+}
+
+export default App
